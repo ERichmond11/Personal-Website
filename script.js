2f797f2190d437db745bde882ecd5a643eabe6b6
Emmanuel <emman774@hotmail.com>
2025-11-20 18:18:35 -0500

    Brand intro and styling updates

--- script.js
+++ script.js
@@ -22,3 +22,46 @@ function toggleDropdown(id, button) {
     : button.innerHTML.replace('▴', '▾');
 }
 
+//Brand images styling
+
+function nextImage(arrow) {
+    const img = arrow.parentElement.querySelector('.slider-img');
+    const front = img.getAttribute('data-front');
+    const back = img.getAttribute('data-back');
+
+    img.src = img.src.includes(front) ? back : front;
+}
+
+function prevImage(arrow) {
+    const img = arrow.parentElement.querySelector('.slider-img');
+    const front = img.getAttribute('data-front');
+    const back = img.getAttribute('data-back');
+
+    img.src = img.src.includes(back) ? front : back;
+}
+
+const images = {
+  black: ["images/BlackFront.PNG", "images/BlackBack.PNG"],
+  white: ["images/WhiteFront.PNG", "images/WhiteBack.PNG"],
+  green: ["images/GreenFront.PNG", "images/GreenBack.PNG"]
+};
+
+const indexTracker = {
+  black: 0,
+  white: 0,
+  green: 0
+};
+
+function nextImage(color) {
+  indexTracker[color] = (indexTracker[color] + 1) % images[color].length;
+  document.getElementById(`${color}-img`).src = images[color][indexTracker[color]];
+}
+
+function prevImage(color) {
+  indexTracker[color] =
+    (indexTracker[color] - 1 + images[color].length) % images[color].length;
+  document.getElementById(`${color}-img`).src = images[color][indexTracker[color]];
+}
+
+
+
